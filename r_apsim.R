afb51baa530040b615cd78d81eca3830cce059bb
Murilodsv <murilodsv@usp.br>
2018-08-06 12:45:43 -0300

    r_apsim

---
+++ r_apsim.R
@@ -0,0 +1,16 @@
+#----------- Run APSIM from CMD ------------#
+
+#--- Requirements:
+#----- Install R and Apsim on your PC
+#----- Add "C:\Program Files (x86)\APSIM710-r4158\Model" to PATH environment variable of your system
+
+#--- Murilo Vianna
+#-------------------------------------------#
+
+wd    = "D:/Murilo/apsim/sugar"
+ap_fn = "sugar_template.apsim"
+
+#--- Call APSIM
+system(paste0("apsim ",wd,"/",ap_fn), wait = T)
+
+
